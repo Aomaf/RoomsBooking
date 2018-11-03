db5c6643b557ccd2f9a44c21cedef2d9971cc8ae
Aomaf <Aomaf08@gmail.com>
2018-11-03 17:01:49 +0000

    initialize the project

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<!-- Root component -->
+<template>
+    <!-- we have to have one element sround every thing -->
+
+    <div id="app">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'app'
+    }
+
+</script>
+
+<style>
+
+</style>
