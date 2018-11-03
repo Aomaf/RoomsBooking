bce23bb7840e824016b91771b64caafbef90a231
Aomaf <Aomaf08@gmail.com>
2018-11-03 22:45:45 +0000

    add external js and style files

---
+++ src/App.js
@@ -0,0 +1,84 @@
+//import App from ' /App vue'
+
+
+const roomsList = [
+    {
+        img: 'https://picsum photos/250/150/?random',
+        type: 'vip',
+        size: '',
+        info: 'Proactively simplify proactive methods of empowerment vis-a-vis.',
+        number: '',
+        price: '400$'
+    }, {
+        img: 'https://picsum photos/250/150/?random',
+        type: 'vip',
+        size: '',
+        info: 'Proactively simplify proactive methods of empowerment vis-a-vis.',
+        number: '',
+        price: '400$'
+    }, {
+        img: 'https://picsum photos/250/150/?random',
+        type: 'special',
+        size: '',
+        info: 'Proactively simplify proactive methods of empowerment vis-a-vis.',
+        number: '',
+        price: '400$'
+    }, {
+        img: 'https://picsum photos/250/150/?random',
+        type: 'special',
+        size: '',
+        info: 'Proactively simplify proactive methods of empowerment vis-a-vis.',
+        number: '',
+        price: '400$'
+    }, {
+        img: 'https://picsum photos/250/150/?random',
+        type: 'vip',
+        size: '',
+        info: 'Proactively simplify proactive methods of empowerment vis-a-vis.',
+        number: '',
+        price: '400$'
+    }, {
+        img: 'https://picsum photos/250/150/?random',
+        type: 'special',
+        size: '',
+        info: 'Proactively simplify proactive methods of empowerment vis-a-vis.',
+        number: '',
+        price: '400$'
+    }, {
+        img: 'https://picsum photos/250/150/?random',
+        type: 'normal',
+        size: '',
+        info: 'Proactively simplify proactive methods of empowerment vis-a-vis.',
+        number: '',
+        price: '400$'
+    }, {
+        img: 'https://picsum photos/250/150/?random',
+        type: 'special',
+        size: '',
+        info: 'Proactively simplify proactive methods of empowerment vis-a-vis.',
+        number: '',
+        price: '400$'
+    }, {
+        img: 'https://picsum photos/250/150/?random',
+        type: 'normal',
+        size: '',
+        info: 'Proactively simplify proactive methods of empowerment vis-a-vis.',
+        number: '',
+        price: '400$'
+    }, {
+        img: 'https://picsum photos/250/150/?random',
+        type: 'vip',
+        size: '',
+        info: 'Proactively simplify proactive methods of empowerment vis-a-vis.',
+        number: '',
+        price: '400$'
+    }
+];
+export default {
+    data() {
+        return {
+            work: 'Filter By :',
+            rooms: roomsList
+        }
+    }
+}
